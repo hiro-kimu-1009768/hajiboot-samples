760d381f53e9923a408f582c021bee5ec9175f23
kimuhiro2005 <tyuel.shigoto.2005@gmail.com>
2018-09-09 13:16:05 +0900

    20180909 add 1 class CustomerService.java

---
+++ RestWebDev/src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,41 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+	@Autowired
+	CustomerRepository customerRepository;
+
+	//    public Customer save(Customer customer) {
+	//        return customerRepository.save(customer);
+	//    }
+
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	public Customer findOne(Integer id) {
+		return customerRepository.findOne(id);
+	}
+
+	public Customer create(Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	public Customer update(Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	public void delete(Integer id) {
+		customerRepository.delete(id);
+	}
+
+	// その他のメソッドは次章で定義します
+}
