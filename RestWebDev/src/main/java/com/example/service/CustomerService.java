26e9de9aeee83439fd160402a78409459629209c
kimuhiro2005 <tyuel.shigoto.2005@gmail.com>
2018-09-09 14:12:05 +0900

    20180909 restMethod add

--- RestWebDev/src/main/java/com/example/service/CustomerService.java
+++ RestWebDev/src/main/java/com/example/service/CustomerService.java
@@ -1,41 +1,36 @@
 package com.example.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
-	@Autowired
-	CustomerRepository customerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
 
-	//    public Customer save(Customer customer) {
-	//        return customerRepository.save(customer);
-	//    }
+    public List<Customer> findAll() {
+        return customerRepository.findAllOrderByName();
+    }
 
-	public List<Customer> findAll() {
-		return customerRepository.findAll();
-	}
+    public Customer findOne(Integer id) {
+        return customerRepository.findOne(id);
+    }
 
-	public Customer findOne(Integer id) {
-		return customerRepository.findOne(id);
-	}
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
 
-	public Customer create(Customer customer) {
-		return customerRepository.save(customer);
-	}
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
 
-	public Customer update(Customer customer) {
-		return customerRepository.save(customer);
-	}
-
-	public void delete(Integer id) {
-		customerRepository.delete(id);
-	}
-
-	// その他のメソッドは次章で定義します
+    public void delete(Integer id) {
+        customerRepository.delete(id);
+    }
 }
